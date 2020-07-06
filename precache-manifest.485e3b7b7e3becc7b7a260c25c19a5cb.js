d2a8818c2b01c78a617e87bd81014c3271c04d67
Mohamed Hafez <54257768+M-Hafez22@users.noreply.github.com>
2020-07-07 01:58:54 +0200

    Add built files from the islamic-prayer-times project

---
+++ precache-manifest.485e3b7b7e3becc7b7a260c25c19a5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c14390057d327153ebd050d3d00e97",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67c5031a0355e929b3d0",
+    "url": "./static/css/main.a50dff9b.chunk.css"
+  },
+  {
+    "revision": "f8bc9306370c9dbf1c03",
+    "url": "./static/js/2.11980a4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.11980a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67c5031a0355e929b3d0",
+    "url": "./static/js/main.f4bfbfc5.chunk.js"
+  },
+  {
+    "revision": "73205aa2643e4759aa8e",
+    "url": "./static/js/runtime-main.1d873a65.js"
+  }
+]);
